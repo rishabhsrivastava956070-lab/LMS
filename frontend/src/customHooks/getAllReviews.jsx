3d146332fa9ccee4b0ef9caaf9b70e2880ab97eb
Rishabh Srivastava <rishabhsrivastava956070@gmail.com>
2026-02-20 11:03:58 +0530

    Refactor GetAllReviews to use server URL
    Updated the API endpoint to use a server URL for fetching reviews.

--- frontend/src/customHooks/getAllReviews.jsx
+++ frontend/src/customHooks/getAllReviews.jsx
@@ -3,24 +3,29 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setAllReview } from "../redux/reviewSlice";
 
+const serverUrl = "https://lms-84yp.onrender.com"; // 🔥 backend URL
+
 const GetAllReviews = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchAllReviews = async () => {
       try {
-        const result = await axios.get("/api/review/allReview");
+        const result = await axios.get(
+          `${serverUrl}/api/review/allReview`
+        );
+
         dispatch(setAllReview(result.data));
         console.log("All reviews:", result.data);
       } catch (error) {
-        console.error(error);
+        console.error("All reviews error:", error);
       }
     };
 
     fetchAllReviews();
   }, [dispatch]);
 
-  return null; // 👈 required
+  return null;
 };
 
 export default GetAllReviews;
