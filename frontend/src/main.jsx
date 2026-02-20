bdda47cfef463fbbacb28287b425f34c8307e804
Rishabh Srivastava <rishabhsrivastava956070@gmail.com>
2026-02-20 11:14:43 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 
-// 👇 ADD THESE TWO LINES
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000";
+
+axios.defaults.baseURL = "https://lms-84yp.onrender.com";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
