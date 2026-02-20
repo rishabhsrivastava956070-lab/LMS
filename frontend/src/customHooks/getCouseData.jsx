e422c9d8216630ce924da6471637dd29bee4e2e7
Rishabh Srivastava <rishabhsrivastava956070@gmail.com>
2026-02-20 11:02:43 +0530

    Update getCouseData.jsx

--- frontend/src/customHooks/getCouseData.jsx
+++ frontend/src/customHooks/getCouseData.jsx
@@ -1,27 +1,31 @@
 import { useEffect } from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setCourseData } from "../redux/courseSlice";
+
+const serverUrl = "https://lms-84yp.onrender.com"; // 🔥 backend URL
 
 const GetCourseData = () => {
   const dispatch = useDispatch();
-  const { userData } = useSelector((state) => state.user);
 
   useEffect(() => {
     const getAllPublishedCourse = async () => {
       try {
-        const result = await axios.get("/api/course/getpublishedcoures");
+        const result = await axios.get(
+          `${serverUrl}/api/course/getpublishedcoures`
+        );
+
         dispatch(setCourseData(result.data));
         console.log("Published courses:", result.data);
       } catch (error) {
-        console.error(error);
+        console.error("Published course error:", error);
       }
     };
 
     getAllPublishedCourse();
   }, [dispatch]);
 
-  return null; // 👈 important
+  return null;
 };
 
 export default GetCourseData;
